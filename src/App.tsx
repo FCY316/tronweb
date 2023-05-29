fd939ec826151871f1e45732097d9ecfcdbeb90b
冯承运 <790322000@qq.com>
2023-05-29 09:48:00 +0800

    ll

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import './App.css';
-import { connectedWalletT, signatureT } from '@/api'
+
+
 function App() {
-  const connectedWallet = async () => {
-    const res = await connectedWalletT()
-    console.log('res', res);
-  }
-  const signature = async () => {
-    const res = await signatureT('你好，世界')
-    console.log('res', res);
 
+    return (
+      <div className='APPEALS'>
+ 
+      </div>
+    );
   }
-  return (
-    <div className="App">
-      <button onClick={connectedWallet}>连接钱包</button>
-      <button onClick={signature}>签名</button>
-    
-    </div>
-  );
-}
+  export default App;
 
-export default App;
