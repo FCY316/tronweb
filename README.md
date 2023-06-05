55dc1f78b2903cb8325b1ffc3256e5ddfddda4ec
冯承运 <790322000@qq.com>
2023-06-05 09:55:17 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,4 @@
+## @tronweb3/tronwallet-adapter-react-hooks;
+## @tronweb3/tronwallet-abstract-adapter;
+## @tronweb3/tronwallet-adapters;
+已目前的测试来看，只会对tronLink兼容性较好，对其他的钱包，连接上很过分的兼容
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './App.css';
-
+import { TronLinkAdapter, BitKeepAdapter } from '@tronweb3/tronwallet-adapters';
 
 function App() {
+  const adapters = useMemo(() => [new BitKeepAdapter()], []);
+ const connectedWallet = () => {
+  
+ }
+  return (
+    <div className='APPEALS'>
+       <button onClick={connectedWallet}>连接钱包</button>
+    </div >
+  );
+}
 
-    return (
-      <div className='APPEALS'>
- 
-      </div>
-    );
-  }
-  export default App;
+
+
+export default App;
 
